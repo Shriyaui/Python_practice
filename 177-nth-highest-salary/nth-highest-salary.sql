1ecdd842d72596993b8a18e31f78e3e9113414bc
Shriyaui <ssr65229@gmail.com>
2026-06-10 16:45:09 +0530

    Time: 495 ms (22.39%) | Memory: 0B (100.00%) - LeetSync

--- 177-nth-highest-salary/nth-highest-salary.sql
+++ 177-nth-highest-salary/nth-highest-salary.sql
@@ -2,7 +2,6 @@ CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
   DECLARE offset_val INT;
   SET offset_val = N - 1;
-  
   RETURN (
       SELECT IFNULL(
           (SELECT DISTINCT salary 
