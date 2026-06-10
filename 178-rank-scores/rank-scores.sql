429513a9cda5a6c72377c8929ad56f07b1ce4463
Shriyaui <ssr65229@gmail.com>
2026-06-10 16:48:25 +0530

    Time: 344 ms (29.14%) | Memory: 0B (100.00%) - LeetSync

--- 178-rank-scores/rank-scores.sql
+++ 178-rank-scores/rank-scores.sql
@@ -1,9 +1,5 @@
 SELECT 
     score,
-    (
-        SELECT COUNT(DISTINCT score)
-        FROM Scores
-        WHERE score >= s.score
-    ) AS 'rank'
-FROM Scores s
+    DENSE_RANK() OVER (ORDER BY score DESC) AS 'rank'
+FROM Scores
 ORDER BY score DESC;
