cc50782374b19264c23ad005d4a1a407d846f363
Shriyaui <ssr65229@gmail.com>
2026-06-17 18:00:00 +0530

    Time: 569 ms (50.83%) | Memory: 0B (100.00%) - LeetSync

--- 1877-find-followers-count/find-followers-count.sql
+++ 1877-find-followers-count/find-followers-count.sql
@@ -1,3 +1,5 @@
-
-select user_id,count(*) as followers_count from Followers group by user_id 
-order by user_id;
+# Write your MySQL query statement below
+SELECT user_id, COUNT(user_id) AS followers_count
+FROM Followers
+GROUP BY user_id
+ORDER BY user_id ASC;
